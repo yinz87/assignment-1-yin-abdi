f503885186042a258dbd7e2fa6f285b0bcfb19d3
Timtro <timtro@gmail.com>
2017-09-20 14:39:06 -0400

    Added euler_step and n_steps for Euler integration.

--- projectile.cpp
+++ projectile.cpp
@@ -1,3 +1,33 @@
 #include "projectile.hpp"
 
-#include <cstdio>
+const double m = 1;
+const double g = 9.8066;
+const double dt = 0.001;
+
+auto force(TState s) { return VecR2{0, -m * g}; }
+
+auto euler_step(TState s, VecR2 accel) {
+  TState next;
+  next.t = s.t + dt;
+  next.position = s.position + (s.velocity * dt);
+  next.velocity = s.velocity + (accel * dt);
+  return next;
+}
+
+void n_steps(unsigned n, TState state0) {
+  print_tstate(state0);
+  if (n == 0)
+    return;
+  else {
+    auto state = state0;
+    for (unsigned k = 0; k < n; ++k) {
+      state = euler_step(state, force(state) / m);
+      print_tstate(state);
+    }
+  }
+}
+
+int main() {
+  n_steps(9000, TState{0., {0, 0.1}, {5, 5}});
+  return 0;
+}
