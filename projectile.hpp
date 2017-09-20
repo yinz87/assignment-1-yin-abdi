aab96fd343eb0cfaa7d992c442abc12820bb2e27
Timtro <timtro@gmail.com>
2017-09-20 14:31:29 -0400

    Fixed semicolon.

--- projectile.hpp
+++ projectile.hpp
@@ -7,10 +7,7 @@ struct VecR2 {
   double y{0};
 };
 
-auto operator+(VecR2 a, VecR2 b) {
-  return VecR2 { a.x + b.x, a.y + b.y }
-}
-
+auto operator+(VecR2 a, VecR2 b) { return VecR2{a.x + b.x, a.y + b.y}; }
 auto operator*(VecR2 a, double c) { return VecR2{c * a.x, c * a.y}; }
 auto operator*(double c, VecR2 a) { return a * c; }
 
