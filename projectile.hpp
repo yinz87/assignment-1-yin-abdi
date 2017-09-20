c14a22dfa787fb641000c78cfd774c9a3be925ab
Timtro <timtro@gmail.com>
2017-09-20 15:05:18 -0400

    Line order fixed for clarity.

--- projectile.hpp
+++ projectile.hpp
@@ -9,8 +9,8 @@ struct VecR2 {
 
 auto operator+(VecR2 a, VecR2 b) { return VecR2{a.x + b.x, a.y + b.y}; }
 auto operator*(VecR2 a, double c) { return VecR2{c * a.x, c * a.y}; }
-auto operator/(VecR2 a, double c) { return VecR2{a.x / c, a.y / c}; }
 auto operator*(double c, VecR2 a) { return a * c; }
+auto operator/(VecR2 a, double c) { return VecR2{a.x / c, a.y / c}; }
 
 struct TState {
   double t{0};
