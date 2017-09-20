a8f57a14e47727b12554a241bf6536232421319c
timtro <timtro@gmail.com>
2017-09-20 13:50:34 -0400

    Data structures for state.

---
+++ projectile.cpp
@@ -0,0 +1,3 @@
+#include "projectile.hpp"
+
+#include <cstdio>
---
+++ projectile.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdio>
+
+struct VecR2 {
+  double x{0};
+  double y{0};
+};
+
+auto operator+(VecR2 a, VecR2 b) {
+  return VecR2 { a.x + b.x, a.y + b.y }
+}
+
+auto operator*(VecR2 a, double c) { return VecR2{c * a.x, c * a.y}; }
+auto operator*(double c, VecR2 a) { return a * c; }
+
+struct TState {
+  double t{0};
+  VecR2 position{0, 0};
+  VecR2 velocity{0, 0};
+};
+
+void print_tstate(TState s) {
+  printf("%f %f %f %f %f\n", s.t, s.position.x, s.position.y, s.velocity.x,
+         s.velocity.y);
+}
