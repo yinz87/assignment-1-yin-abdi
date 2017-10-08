a4817d3544624a5cd430b6b2e2026ce6aa1fa390
Yin Zhou <31861121+yinz87@users.noreply.github.com>
2017-10-08 16:19:58 -0400

    snytax correction

--- assignment1.cpp
+++ assignment1.cpp
@@ -1,15 +1,15 @@
-#include "projectile.hpp"
+#include "assignment1.hpp"
 
 const double dt = 0.001;
 
-auto acceleration(TState s) { return VecR3<double>{0,  ,0}; }
+auto force(TState s) { return VecR3<double>{cos(k*s.position.z-w*s.t), sin(k*s.position.z-w*s.t) ,0}; }
 
 auto Verlet_step(TState s, VecR3<double> accel) {
   TState next;
   next.t = s.t + dt;
-  next.position = s.position + (s.velocity * dt) + 1/2 * accel*pow(dt,2);
+  next.position = s.position + (s.velocity * dt) + .5 * accel*pow(dt,2);
   //verlet equation for x
-  next.velocity = s.velocity + ((accel + accel + accel*dt)/2 * dt);
+  next.velocity = s.velocity + ((accel + accel + accel*dt)/2.0 * dt);
   //verlet equaiton for v
   return next;
 
@@ -22,7 +22,7 @@ void n_steps(unsigned n, TState state0) {
   else {
     auto state = state0;
     for (unsigned k = 0; k < n; ++k) {
-      state = euler_step(state, force(state) / m);
+      state = Verlet_step(state, force(state) / m);
       print_tstate(state);
     }
   }
